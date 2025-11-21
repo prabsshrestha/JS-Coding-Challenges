f6c9fe7311866fcd35747aa096fc17a885a2b2ea
Prabsshrestha <prabs.shrestha@gmail.com>
2025-11-21 18:59:38 +0545

    Solved q5: Vairbale scope

---
+++ javascript-basics/q5.js
@@ -0,0 +1,17 @@
+//Q5 Variable Scope Demo
+function scopeDemo() {
+  function fn() {
+    var x = 'var'; 
+    let y = 'let'; 
+    const z = 'const'; 
+    if (true) {
+      var x2 = 'var-in-block';
+      let y2 = 'let-in-block';
+      const z2 = 'const-in-block';
+      console.log('inside block:', x2, y2, z2);
+    }
+    console.log('inside function:', x, y, z);
+  }
+  fn();
+}
+scopeDemo();
