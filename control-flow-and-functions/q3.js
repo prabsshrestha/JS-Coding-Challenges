97c571ace44fcb2fd3a24621528f243170d0c4bd
Prabsshrestha <prabs.shrestha@gmail.com>
2025-11-21 19:29:30 +0545

    Solved q3: Reverse Array using for..of

--- control-flow-and-functions/q3.js
+++ control-flow-and-functions/q3.js
@@ -1,8 +1,8 @@
 // Q3 Reverse Array
 function reverseArray(arr) {
   const res = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    res.push(arr[i]);
+  for (const v of arr) {
+    res.unshift(v);
   }
   return res;
 }
